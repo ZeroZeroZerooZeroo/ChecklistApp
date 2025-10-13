d0048324970fad390b90de8bd9a27b2d13b1cfbb
ZeroZeroZerooZeroo <misha.solodownick@yandex.ru>
2025-10-13 19:04:58 +0700

    обновлен main API сервиса

--- APIService/cmd/apiservice/main.go
+++ APIService/cmd/apiservice/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/ZeroZeroZerooZeroo/ChecklistApp/apiservice/internal/grpc"
 	"github.com/ZeroZeroZerooZeroo/ChecklistApp/apiservice/internal/handlers"
 	"github.com/ZeroZeroZerooZeroo/ChecklistApp/apiservice/internal/routes"
 )
@@ -11,13 +13,25 @@ import (
 func main() {
 	// TODO: инициализация конфига
 
-	// TODO: инициализация хендлера
-	handler := handlers.NewHandler()
+	//Временно пока без конфига
+	grpcServerAddress := "localhost:50051"
+
+	grpcClient, err := grpc.NewGRPCClient(grpcServerAddress)
+	if err != nil {
+		log.Fatalf("Failed to connect to gRPC server: %v", err)
+	}
+	defer grpcClient.Close()
+
+	fmt.Printf("Connected to gRPC server at %s\n", grpcServerAddress)
+
+	// Инициализация хендлера с gRPC клиентом
+	handler := handlers.NewHandler(grpcClient)
 
 	router := routes.SetupRouters(handler)
 
 	fmt.Println("Server started on :8080")
-	http.ListenAndServe(":8080", router)
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 
-	// TODO: запуск приложения
 }
