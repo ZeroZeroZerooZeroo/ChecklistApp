6b2c2b16fd13427675309c6fd52f35af9ed2940e
ZeroZeroZerooZeroo <misha.solodownick@yandex.ru>
2025-10-13 20:54:31 +0700

    Добавлена функция создания задачи. Добавлены функции для преобразования в ответы gRPC

--- DataBaseService/go.mod
+++ DataBaseService/go.mod
@@ -5,12 +5,19 @@ go 1.25.1
 require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	golang.org/x/net v0.41.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250707201910-8d1bb00bc6a7 // indirect
+	google.golang.org/grpc v1.75.1 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
 )
 
 require (
 	github.com/golang-migrate/migrate/v4 v4.19.0
 	github.com/lib/pq v1.10.9
 )
+
+require github.com/ZeroZeroZerooZeroo/ChecklistApp/proto v0.0.0
 
 replace github.com/ZeroZeroZerooZeroo/ChecklistApp/proto => ../proto
--- DataBaseService/internal/handler/handler.go
+++ DataBaseService/internal/handler/handler.go
@@ -1,1 +1,62 @@
 package handler
+
+import (
+	"context"
+	"log"
+
+	"github.com/ZeroZeroZerooZeroo/ChecklistApp/databaseservice/internal/models"
+	"github.com/ZeroZeroZerooZeroo/ChecklistApp/databaseservice/internal/service"
+	pb "github.com/ZeroZeroZerooZeroo/ChecklistApp/proto/checklist/pb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type GRPCHandler struct {
+	pb.UnimplementedCheckListServiceServer
+	taskService *service.Service
+}
+
+func NewGRPCHandler(taskService *service.Service) *GRPCHandler {
+	return &GRPCHandler{
+		taskService: taskService,
+	}
+}
+
+func (h *GRPCHandler) CreateTask(ctx context.Context, req *pb.CreateTaskRequest) (*pb.TaskResponse, error) {
+	log.Printf("Creating task:%s", req.Title)
+
+	task, err := h.taskService.CreateTask(req.Title, req.Description)
+	if err != nil {
+		log.Printf("Failed to create task:%v", err)
+		return nil, status.Error(codes.Internal, "failed to create task")
+
+	}
+
+	log.Printf("Task created with ID:%d", task.ID)
+	return h.taskToProto(task), nil
+
+}
+
+func (h *GRPCHandler) taskToProto(task *models.Task) *pb.TaskResponse {
+	return &pb.TaskResponse{
+		Id:          int32(task.ID),
+		Title:       task.Title,
+		Description: task.Description,
+		IsCompleted: task.IsCompleted,
+		CreatedAt:   timestamppb.New(task.CreatedAt),
+		UpdatedAt:   timestamppb.New(task.UpdatedAt),
+	}
+}
+
+func (h *GRPCHandler) tasksToProto(tasks []*models.Task) *pb.TaskListResponse {
+	protoTask := make([]*pb.TaskResponse, 0, len(tasks))
+	for _, task := range tasks {
+		protoTask = append(protoTask, h.taskToProto(task))
+	}
+
+	return &pb.TaskListResponse{
+		Tasks: protoTask,
+		Count: int32(len(protoTask)),
+	}
+}
