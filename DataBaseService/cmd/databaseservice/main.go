754d22760ebf6b9e3528799ed37275fd90a35069
ZeroZeroZerooZeroo <misha.solodownick@yandex.ru>
2025-10-13 21:12:38 +0700

    Реализован main файл

--- DataBaseService/cmd/databaseservice/main.go
+++ DataBaseService/cmd/databaseservice/main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"log"
+	"net"
+
+	"github.com/ZeroZeroZerooZeroo/ChecklistApp/databaseservice/internal/config"
+	"github.com/ZeroZeroZerooZeroo/ChecklistApp/databaseservice/internal/handler"
+	"github.com/ZeroZeroZerooZeroo/ChecklistApp/databaseservice/internal/repository"
+	"github.com/ZeroZeroZerooZeroo/ChecklistApp/databaseservice/internal/service"
+	"github.com/ZeroZeroZerooZeroo/ChecklistApp/databaseservice/pkg/database"
+	"github.com/ZeroZeroZerooZeroo/ChecklistApp/proto/checklist/pb"
+	"google.golang.org/grpc"
+)
+
 func main() {
 
+	cfg := config.LoadConfig()
+
+	db, err := database.NewDatabase(cfg.DatabaseURL)
+
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	defer func() {
+		log.Println("Closing database connection")
+		db.Close()
+	}()
+
+	if err := database.RunMigrations(cfg.DatabaseURL); err != nil {
+		log.Fatalf("Failed to run migrations: %v", err)
+	}
+
+	repo := repository.NewRepository(db.DB)
+	serv := service.NewService(repo)
+	grpchandler := handler.NewGRPCHandler(serv)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCheckListServiceServer(grpcServer, grpchandler)
+
+	lis, err := net.Listen("tcp", cfg.GRPCPort)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	log.Printf("DataBaseService (gRPC server) started on port %s", cfg.GRPCPort)
+	log.Printf("Database: %s", cfg.DatabaseURL)
+
+	 if err := grpcServer.Serve(lis); err != nil {
+        log.Fatalf("Failed to serve: %v", err)
+    }
 }
