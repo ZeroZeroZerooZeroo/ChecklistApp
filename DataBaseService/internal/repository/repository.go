35db7c1410d2e226648f7a16591cc70b45dae120
ZeroZeroZerooZeroo <misha.solodownick@yandex.ru>
2025-10-13 20:02:03 +0700

    Добавлена функция получения всех задач

--- DataBaseService/internal/repository/repository.go
+++ DataBaseService/internal/repository/repository.go
@@ -35,6 +35,42 @@ func (r *Repository) CreateTask(title, description string) (*models.Task, error)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create task: %w", err)
 
-		return &task, nil
 	}
+	return &task, nil
+}
+
+func (r *Repository) GetAllTasks() ([]*models.Task, error) {
+	query := `SELECT id,title,description,is_completed,created_at,updated_at
+	FROM tasks`
+
+	rows, err := r.db.Query(query)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get tasks: %w:", err)
+	}
+
+	defer rows.Close()
+
+	var tasks []*models.Task
+	for rows.Next() {
+		var task models.Task
+		err := rows.Scan(
+			&task.ID,
+			&task.Title,
+			&task.Description,
+			&task.IsCompleted,
+			&task.CreatedAt,
+			&task.UpdatedAt,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("failed to scan task: %w", err)
+
+		}
+		tasks = append(tasks, &task)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("error inerating tasks: %w", err)
+
+	}
+
+	return tasks, nil
 }
